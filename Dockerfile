d93b0c9df9a5206b1c6cd50d774d0e72d99cdb14
Sudalaimani Sundararaj <PT6903@CLUSTER.COM>
2025-04-07 21:19:40 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:23-alpine3.20
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+EXPOSE 3000
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('CI/CD with GitHub Actions, Node.js, and Docker 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
